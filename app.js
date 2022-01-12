fe3f9015d3bf1ba639d47bd5fe9e870acfed3632
arttuhar <arttuhar@gmail.com>
2022-01-12 19:59:23 +0200

    Add id's for html elements, js variabled declared

---
+++ app.js
@@ -0,0 +1,9 @@
+const bankBalance = document.getElementById("bankBalance");
+const loanButton = document.getElementById("loanButton");
+const payBalance = document.getElementById("payBalance");
+const bankButton = document.getElementById("bankButton");
+const workButton = document.getElementById("workButton");
+
+let bankTotal = 0;
+let payTotal = 0;
+let laptops = [];
